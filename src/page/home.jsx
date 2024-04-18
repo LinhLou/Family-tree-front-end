63ebfbad04e766851fc2d431414e50526e6786c6
linh lou <dang.tn.linh@gmail.com>
2024-04-18 15:56:57 +0200

    setup home page

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+  const user = useSelector(state=>state.user);
+  console.log(user);
   return (
-    <div>Home</div>
+    <div className='d-flex justify-content-center align-items-center mt-5'>
+      <div className='d-flex justify-content-center align-items-center gap-5 mt-5 w-50'>
+        <a className="btn btn-primary d-block w-50" href="/login" role="button">Log in</a>
+        <a className="btn btn-primary d-block w-50" href="/signup" role="button">Sign up</a>
+      </div>
+    </div>
   )
 }
