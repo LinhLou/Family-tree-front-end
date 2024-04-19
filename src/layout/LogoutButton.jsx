3b5fc83ddd26d2040d3131a00d36d0da9131b933
linh lou <dang.tn.linh@gmail.com>
2024-04-19 09:56:41 +0200

    resetStore when user is not connected (at login page and signup page)

--- src/layout/LogoutButton.jsx
+++ src/layout/LogoutButton.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { localStorageName } from '../data/constant';
 import { resetStore } from '../services/redux/resetStore';
 
 export default function LogoutButton() {
   const navigate = useNavigate();
   function handleClick(){
     resetStore();
-    localStorage.removeItem(`${localStorageName}`);
     navigate('/');
   }
   return (
--- src/services/redux/resetStore.js
+++ src/services/redux/resetStore.js
@@ -1,6 +1,8 @@
 import { initUser } from "../../features/Authentification";
 import store from './store';
+import { localStorageName } from "../../data/constant";
 
 export function resetStore(){
   store.dispatch(initUser());
+  localStorage.removeItem(`${localStorageName}`);
 }
