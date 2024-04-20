4ad0835d3ec195fb9c08d3458bda8b1567c26b01
linh lou <dang.tn.linh@gmail.com>
2024-04-20 11:24:01 +0200

    import logo image for showing correctly in LayoutVisiter

--- src/layout/layoutVisiter.jsx
+++ src/layout/layoutVisiter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { resetStore } from '../services/redux/resetStore';
+import logo from '../assets/react.svg'
 
 export default function Layout() {
   resetStore()
@@ -9,7 +10,7 @@ export default function Layout() {
       <nav className="navbar bg-body-tertiary">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            <img src="./src/assets/react.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+            <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
             Name App
           </Link>
         </div>
